775e29e0d7667ade681eb59e35d7533ff5c32d9c
zhoutk <zhoutk@189.cn>
2017-12-06 09:01:28 +0800

    简单查询功能完成。

--- dbhelper.py
+++ dbhelper.py
@@ -1,26 +1,38 @@
 import pymysql
-from functools import reduce
+import json
 
-conn = pymysql.connect()
+with open("./configs.json", 'r', encoding='utf-8') as json_file:
+    dbconf = json.load(json_file)['db_config']
+
+conn = pymysql.connect(host=dbconf['db_host'], port=dbconf['db_port'], user=dbconf['db_username'],
+                       passwd=dbconf['db_password'], db=dbconf['db_database'], charset=dbconf['db_charset'])
 
 
 def exec_sql(sql, values):
     try:
         with conn.cursor() as cursor:
             cursor.execute(sql, values)
-        result = cursor.fetchone()
+        result = cursor.fetchall()
         # conn.commit()
     finally:
         conn.close()
     return result
 
 
-def composeWhere(x):
-    return
-
-
-def select(tablename, params = {}, fields = []):
-    sql = "select * from %s "
+def select(tablename, params={}, fields=[]):
+    sql = "select %s from %s " % ('*' if len(fields) == 0 else ','.join(fields), tablename)
     ks = params.keys()
+    where = " where "
+    ps = []
+    pvs = []
     if len(ks) > 0:
-        where = reduce(fn, ks)
+        for al in ks:
+            ps.append(al + " =%s ")
+            pvs.append(params[al])
+        where += ' and '.join(ps)
+
+    rs = exec_sql(sql+where, pvs)
+    print(len(rs), rs)
+
+
+select('member', {'username': 'admin', "status": 1})
